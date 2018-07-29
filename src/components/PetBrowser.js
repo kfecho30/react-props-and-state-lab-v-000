2ec2529e7c51e5831d91fd39f1ef3935e4691870
Kelsey Fecho <kfecho30@gmail.com>
2018-07-29 15:20:38 +0000

    automatically backed up by learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -6,7 +6,6 @@ class PetBrowser extends React.Component {
   render() {
     const pets = this.props.pets.map((pet, index) => {
       return <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} />
-  }
 }
 
 export default PetBrowser
